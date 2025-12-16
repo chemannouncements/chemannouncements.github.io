f0382fcd04e634f2c886e11b71b14696f13ed5c0
chemannouncements <chemannounce@gmail.com>
2025-12-16 10:55:59 -0800

    Update molecule.js

--- molecule.js
+++ molecule.js
@@ -1,5 +1,33 @@
 // Random molecule display from PubChem
 
+// Filtered CIDs (hashed for discretion)
+const FILTERED_HASHES = new Set([
+  '7e66b5', '2069cb', 'f68a11', 'fc7b52', '261b6c', '1c8dcc', '0a990e',
+  '1afc50', '3b4f36', '3bad45', 'f393ed', 'c344fd', '6ffdf0', '92ffee',
+  'ffe418', '0d551e', '01c062', 'bb602e', '39c7ad', 'ee6f01', '5f6b81',
+  '323e37', '55b804', '9763a4', '87595d', '2ac913', '25375a', '26fdca', '9e0cc3'
+]);
+
+async function hashStr(str) {
+  const data = new TextEncoder().encode(str.toLowerCase());
+  const buf = await crypto.subtle.digest('SHA-256', data);
+  return Array.from(new Uint8Array(buf)).slice(0, 3)
+    .map(b => b.toString(16).padStart(2, '0')).join('');
+}
+
+async function isFiltered(cid, name) {
+  const cidHash = await hashStr(String(cid));
+  if (FILTERED_HASHES.has(cidHash)) return true;
+  
+  const words = name.toLowerCase().split(/[\s\-\_]+/);
+  for (const word of words) {
+    if (word.length < 4) continue;
+    const h = await hashStr(word);
+    if (FILTERED_HASHES.has(h)) return true;
+  }
+  return false;
+}
+
 function formatFormula(formula) {
   return formula.replace(/(\d+)/g, '<sub>$1</sub>');
 }
@@ -12,9 +40,15 @@ function loadMolecule(containerId = 'molecule') {
 
   fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/${cid}/property/Title,MolecularFormula,MolecularWeight/JSON`)
     .then(r => r.json())
-    .then(data => {
+    .then(async data => {
       const props = data.PropertyTable.Properties[0];
       const name = props.Title || '';
+
+      // Skip if filtered
+      if (await isFiltered(cid, name)) {
+        loadMolecule(containerId);
+        return;
+      }
 
       // Skip if name is too long or missing
       if (!name || name.length > 36) {
